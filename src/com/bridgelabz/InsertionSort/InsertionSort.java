532b2ba5fc3cfb9c0ce077d851d5516fbd39f12e
MayaChandre <84507177+MayaChandre@users.noreply.github.com>
2021-07-07 03:52:08 -0400

    Update InsertionSort.java

--- src/com/bridgelabz/InsertionSort/InsertionSort.java
+++ src/com/bridgelabz/InsertionSort/InsertionSort.java
@@ -2,19 +2,21 @@ package com.bridgelabz.InsertionSort;
 
 public class InsertionSort {
 
-	public static void insertionSort(int array[]) {  
+	public static void insertionSort(int array[])
+	{  
         int n = array.length;  
         for (int j = 1; j < n; j++) {  
             int key = array[j];  
             int i = j-1;  
-            while ( (i > -1) && ( array [i] > key ) ) {  
+            while ( (i > -1) && ( array [i] > key ) )
+	    {  
                 array [i+1] = array [i];  
                 i--;  
             }  
             array[i+1] = key;  
         }  
-    }  
-       
+    } 
+
     public static void main(String a[]){    
         int[] arr1 = {9,14,3,2,43,11,58,22};    
         System.out.println("Before Insertion Sort");    
@@ -23,7 +25,9 @@ public class InsertionSort {
         }    
         System.out.println();    
             
-        insertionSort(arr1);//sorting array using insertion sort    
+        insertionSort(arr1);
+	    
+	//sorting array using insertion sort    
            
         System.out.println("After Insertion Sort");    
         for(int i:arr1){    
