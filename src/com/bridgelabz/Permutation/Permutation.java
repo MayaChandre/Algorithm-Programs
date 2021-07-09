45a7c4f6eb4ecf9a01df9127413853864ab4f9a4
mayavathi <mayachandre27@gmail.com>
2021-07-09 07:43:29 -0400

    two strings are equal or not

--- src/com/bridgelabz/Permutation/Permutation.java
+++ src/com/bridgelabz/Permutation/Permutation.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class Permutation {
 
 	//main 
-	
-	public static void main(String[] args) {     
+		public static void main(String[] args) {     
         System.out.println("Enter the string: ");
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
